97431e9c7d770b31a8b250419083eb3d69ab030b
Musiker15 <info@musiker15.de>
2026-04-11 14:44:55 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Chezza-Rework/chezza-documentation/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Chezza-Rework/chezza-documentation/tree/main/',
         },
         blog: false,
         theme: {
